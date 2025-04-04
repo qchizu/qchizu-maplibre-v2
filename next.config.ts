80dab5615cb914ada1521d27793f463bd716a56f
johofukyu <johofukyu@users.noreply.github.com>
2025-04-04 13:11:03 +0900

    Revert "ワークフロー、ファビコンを更新"

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    reactStrictMode: true,
+    basePath: '/3dpc-3dtiles',
+    assetPrefix: '/3dpc-3dtiles',
     output: "export",
     experimental: {
         optimizePackageImports: ["@chakra-ui/react"],
