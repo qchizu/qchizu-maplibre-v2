be02c3e5cad10bdbc9a27c8d40518eb1334f8d11
himagone <sunfgirl1975@icloud.com>
2025-05-02 08:37:59 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
 
-const isPreview = process.env.IS_PREVIEW === "true";
-const prNumber = process.env.PR_NUMBER;
-const basePath = isPreview ? `/pr-preview-${prNumber}` : process.env.BASE_PATH || "";
-
 const nextConfig: NextConfig = {
     output: "export",
-    basePath: basePath,
-    assetPrefix: isPreview ? `${basePath}/` : undefined,
+    basePath: process.env.BASE_PATH,
     experimental: {
         optimizePackageImports: ["@chakra-ui/react"],
     },
